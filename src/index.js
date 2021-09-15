03374eb72c85e0fad578ad109e1879de663ec362
Danlois Tovar <danlois.tovar@gmail.com>
2021-09-14 23:44:57 -0300

    Inicio de proyecto de gestor de proyectos.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/* llamar a express: */
+const express = require('express');
+/* crear aplicacion de express: */
+const app = express();
+/* puerto: */
+const port = 3000;
+
+/* ruta principal: */
+app.use('/', (req, res) => {
+  res.send('<h1>Hola desde nodejs!</h1>');
+});
+
+/* servidor escucha: */
+app.listen(port, () => {
+  console.log(`Servidor escuchando por el puerto ${3000}!!!`);
+});
